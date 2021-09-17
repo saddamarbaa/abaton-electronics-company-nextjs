a29d4dcb6e297723e36049c8653f81fbc7ee7677
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-09-17 23:40:07 +0700

    fix: vercel error

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import React from "react";
+import React, { Fragment } from "react";
 import MainNavigation from "./MainNavigation";
 import classes from "./Layout.module.css";
 import Footer from "./Footer";
@@ -11,7 +11,7 @@ import Contact from "../Contact";
 
 const Layout = (props) => {
 	return (
-		<>
+		<Fragment>
 			<MainNavigation />
 			<main className={classes.main}>
 				<About />
@@ -20,7 +20,7 @@ const Layout = (props) => {
 				<Contact />
 				<Footer />
 			</main>
-		</>
+		</Fragment>
 	);
 };
 
