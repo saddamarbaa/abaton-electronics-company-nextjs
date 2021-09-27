283dd14a736bec0331f4eda6dec5ab384e62f36f
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-09-27 14:39:42 +0700

    updae: add client real products

--- components/home-page/landing.js
+++ components/home-page/landing.js
@@ -5,8 +5,6 @@ import classes from "./landing.module.css";
 
 import { useRouter } from "next/router";
 
-
-
 const Landing = () => {
 	const router = useRouter();
 
@@ -14,68 +12,69 @@ const Landing = () => {
 		router.push("/product");
 	};
 	return (
-		<section className={classes.landing}>
-			<div className={classes.text}>
-				<h2>AGEO 19</h2>
+		<section className={classes.wrapper}>
+			<div className={classes.landing}>
+				<div className={classes.text}>
+					<h2>AGEO 19</h2>
+					<div>
+						<p>
+							People Count and <span>occupancy solutions</span>
+						</p>
+
+						<p>
+							<Image
+								alt='svg logo'
+								src='/images/svsgg.svg'
+								objectFit='contain'
+								width={30}
+								height={30}
+								style={{ display: "block", marginRight: "10px" }}
+							/>
+							<span style={{ marginLeft: "7px" }}>
+								Vital during the Covid-19 pandemic
+							</span>
+						</p>
+					</div>
+					<button className={classes.show} onClick={handleClick}>
+						Check the product
+					</button>
+				</div>
 				<div>
-					<p>
-						People Count and <span>occupancy solutions</span>
-					</p>
-
-					<p>
+					<div
+						className={classes.image}
+						style={{
+							position: "relative",
+						}}
+						onClick={handleClick}>
 						<Image
-							alt='svg logo'
-							src='/images/svsgg.svg'
-							objectFit='contain'
-							width={30}
-							height={30}
-							style={{ display: "block", marginRight: "10px" }}
+							src='/images/facetwo.png'
+							alt='product Image'
+							layout='fill'
+							objectFit='cover'
 						/>
-						<span style={{ marginLeft: "7px" }}>
-							Vital during the Covid-19 pandemic
-						</span>
-					</p>
-				</div>
-				<button className={classes.show} onClick={handleClick}>
-					Check the product
-				</button>
-			</div>
-			<div>
-				<div
-					className={classes.image}
-					style={{
-						position: "relative",
-					}}
-					onClick={handleClick}>
-					<Image
-						src='/images/sensor.png'
-						alt='product Image'
-						layout='fill'
-						objectFit='contain'
-					/>
-				</div>
+					</div>
 
-				<div
-					className={classes.image}
-					style={{
-						position: "relative",
-					}}
-					onClick={handleClick}>
-					<Image
-						src='/images/facetwo.png'
-						alt='product Image'
-						layout='fill'
-						objectFit='cover'
-					/>
+					<div
+						className={classes.image}
+						style={{
+							position: "relative",
+						}}
+						onClick={handleClick}>
+						<Image
+							src='/images/product.jpg'
+							alt='product Image'
+							layout='fill'
+							objectFit='cover'
+						/>
+					</div>
+
+					<button onClick={handleClick} className={classes.hidden}>
+						Check the product
+					</button>
 				</div>
-				<button onClick={handleClick} className={classes.hidden}>
-					Check the product
-				</button>
 			</div>
 		</section>
 	);
 };
 
 export default Landing;
-
-
